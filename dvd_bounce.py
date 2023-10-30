49793c60afcda6576aa506f9ae03525c5657045e
Isabelle <meguminperson@gmail.com>
2023-10-30 12:44:57 +0000

    feat: Added confetti on corner bounce

--- dvd_bounce.py
+++ dvd_bounce.py
@@ -16,6 +16,9 @@ INITIAL_TILE_MOVEMENT_SPEED: Final[float] = 2
 RANDOM_SPEED_DELTA: Final[float] = 0.1
 RANDOM_SIZE_DELTA: Final[float] = 2
 STARTING_NUMBER_OF_SPRITES: Final[int] = 1
+CONFETTI_PARTICLE_SIZE: Final[int] = 5
+CONFETTI_PARTICLE_SPEED: Final[int] = 5
+NUMBER_OF_CONFETTI_PARTICLES: Final[int] = 200
 
 BASE_IMAGE: Final[pygame.Surface] = pygame.image.load("dvd_logo.png")
 IMAGE_SCALE_FACTOR: Final[float] = max(max(BASE_IMAGE.get_size()) / IMG_MAX_SIZE, 1)
@@ -61,6 +64,26 @@ def make_ball() -> SpriteAsset:
     return ball
 
 
+def rand_colour() -> Tuple[int, int, int]:
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return (r, g, b)
+
+
+class Particle:
+    def __init__(self, starting_pos: Tuple[float, float], vector: pygame.math.Vector2) -> None:
+        self.pos: Tuple[float, float] = starting_pos
+        self.vector = vector
+        self.colour = rand_colour()
+
+    def update_pos(self) -> None:
+        self.pos = (
+            self.pos[0] + uniform(self.vector.x, self.vector.x*2),
+            self.pos[1] + uniform(self.vector.y, self.vector.y*2),
+        )
+
+
 def main() -> None:
     pygame.init()
 
@@ -76,6 +99,8 @@ def main() -> None:
     sprite_list: List[SpriteAsset] = []
     speed_multiplier: float = 1
     pygame.key.set_repeat(600, 60)
+
+    confetti_particles: List[Particle] = []
 
     for _ in range(STARTING_NUMBER_OF_SPRITES):
         sprite = make_ball()
@@ -110,26 +135,29 @@ def main() -> None:
             # Bounce the sprites if needed: walls
             y_collided = False
             x_collided = False
+            confetti_delta_x = 0
+            confetti_delta_y = 0
             normal_vector = None
             if sprite.object.y > SCREEN_HEIGHT - sprite.pygame_image_size[1]:
                 sprite.object.change_y = abs(sprite.object.change_y)
                 normal_vector = pygame.Vector2(0, -1)
                 y_collided = True
+                confetti_delta_y = -1
             elif sprite.object.y < 0:
                 sprite.object.change_y = -abs(sprite.object.change_y)
                 normal_vector = pygame.Vector2(0, 1)
                 y_collided = True
+                confetti_delta_y = 1
             if sprite.object.x > SCREEN_WIDTH - sprite.pygame_image_size[0]:
                 sprite.object.change_x = abs(sprite.object.change_x)
                 normal_vector = pygame.Vector2(-1, 0)
                 x_collided = True
+                confetti_delta_x = -1
             elif sprite.object.x < 0:
                 sprite.object.change_x = -abs(sprite.object.change_x)
                 normal_vector = pygame.Vector2(1, 0)
                 x_collided = True
-
-            if y_collided and x_collided:
-                print("CORNER BOUNCE")
+                confetti_delta_x = 1
 
             if normal_vector:
                 normal_vector.rotate_ip(randint(-3, 3))
@@ -137,6 +165,18 @@ def main() -> None:
                 reflect_vector = move_vector.reflect(normal_vector)
                 sprite.object.change_x = reflect_vector.x
                 sprite.object.change_y = reflect_vector.y
+                particle_vector = pygame.math.Vector2(
+                    CONFETTI_PARTICLE_SPEED * speed_multiplier,
+                    CONFETTI_PARTICLE_SPEED * speed_multiplier,
+                )
+                if y_collided and x_collided:
+                    for _ in range(NUMBER_OF_CONFETTI_PARTICLES):
+                        confetti_particles.append(
+                            Particle(
+                                (sprite.object.x, sprite.object.y),
+                                particle_vector.rotate(randrange(360)),
+                            ),
+                        )
 
         screen.fill(WHITE)
 
@@ -148,6 +188,29 @@ def main() -> None:
                 sprite.pygame_image_size[1],
             )
             screen.blit(sprite.pygame_image, dvd_logo_rect)
+
+        for particle in confetti_particles:
+            particle.update_pos()
+            offscreen_buffer = 100 # stop instant despawn
+            particle_is_on_screen = (
+                particle.pos[0] <= (SCREEN_DIMENSIONS[0] + offscreen_buffer)
+                and particle.pos[1] <= (SCREEN_DIMENSIONS[1] + offscreen_buffer)
+                and particle.pos[0] >= -offscreen_buffer
+                and particle.pos[1] >= -offscreen_buffer
+            )
+            if not particle_is_on_screen:
+                confetti_particles.remove(particle)
+            else:
+                pygame.draw.rect(
+                    screen,
+                    particle.colour,
+                    (
+                        particle.pos[0],
+                        particle.pos[1],
+                        CONFETTI_PARTICLE_SIZE,
+                        CONFETTI_PARTICLE_SIZE,
+                    ),
+                )
 
         clock.tick(60)
 
